559b192b2c9007abd57944c46aef972a20e2d1d6
Max_T <maximtarnovsky@gmail.com>
2024-08-31 14:58:00 +0300

    get function sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -7,3 +7,10 @@ def filter_by_state(list_data: list, key: str="EXECUTED") -> list:
         if i["state"] == key:
             new_list.append(i)
     return new_list
+
+
+def sort_by_date(sorted_list: list, reverse: bool = True) -> list:
+    """Функция, принимающая список словарей и необязательный параметр,
+    задающий порядок сортировки (по умолчанию — убывание) и возвращающая
+    новый список, отсортированный по дате (date)."""
+    return sorted(sorted_list, key=lambda x: x["date"], reverse=reverse)
